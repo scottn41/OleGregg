0844855b53e1cbe6cb1942661d0b3a44270ac0d8
tduncan <tmduncan@gmail.com>
2017-01-24 13:35:09 -0800

    Duh

---
+++ src/main/java/com/olegregg/OleGreggApplication.java
@@ -0,0 +1,12 @@
+package com.olegregg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OleGreggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OleGreggApplication.class, args);
+	}
+}
