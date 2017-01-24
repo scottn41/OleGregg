b031f5724cc5537088fef6dbe2827ecc5becb22a
tduncan <tmduncan@gmail.com>
2017-01-24 14:43:17 -0800

    blah

--- src/main/java/com/olegregg/domain/Todo.java
+++ src/main/java/com/olegregg/domain/Todo.java
@@ -6,6 +6,7 @@ public class ToDo {
 
     private String guid;
     private ToDoStatus status;
+    private String thing;
     private Date created;
     private Date updated;
 
@@ -39,5 +40,13 @@ public class ToDo {
 
     public void setUpdated(Date updated) {
         this.updated = updated;
+    }
+
+    public String getThing() {
+        return thing;
+    }
+
+    public void setThing(String thing) {
+        this.thing = thing;
     }
 }
